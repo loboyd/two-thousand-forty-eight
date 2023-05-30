0b6a1d10ae6f6e560e0cdf8f506cbfe532e1e177
loboyd <logan.boyd@protonmail.com>
2023-05-30 12:25:30 -0400

    Add leading underscore to "private" method names

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ class Game:
     def move(self, direction):
         if direction == Direction.RIGHT:
             for r in range(4):
-                self.board[r] = reversed(Game.move_unit(reversed(self.board[r])))
+                self.board[r] = reversed(Game._move_unit(reversed(self.board[r])))
         if direction == Direction.LEFT:
             for r in range(4):
-                self.board[r] = Game.move_unit(self.board[r])
+                self.board[r] = Game._move_unit(self.board[r])
         if direction == Direction.UP:
             for c in range(4):
                 unit = [self.board[0][c], self.board[1][c], self.board[2][c], self.board[3][c]]
-                unit = Game.move_unit(unit)
+                unit = Game._move_unit(unit)
                 self.board[0][c] = unit[0]
                 self.board[1][c] = unit[1]
                 self.board[2][c] = unit[2]
@@ -40,7 +40,7 @@ class Game:
         if direction == Direction.DOWN:
             for c in range(4):
                 unit = [self.board[3][c], self.board[2][c], self.board[1][c], self.board[0][c]]
-                unit = Game.move_unit(unit)
+                unit = Game._move_unit(unit)
                 self.board[0][c] = unit[3]
                 self.board[1][c] = unit[2]
                 self.board[2][c] = unit[1]
@@ -56,17 +56,17 @@ class Game:
         self.board[r][c] = 1 if random.random() < 0.9 else 2
 
     @staticmethod
-    def move_unit(unit):
-        unit = Game.collapse(unit)
+    def _move_unit(unit):
+        unit = Game._collapse(unit)
         for i in range(3):
             if unit[i] > 0 and unit[i] == unit[i+1]:
                 unit[i] += 1
                 unit[i+1] = 0
-        unit = Game.collapse(unit)
+        unit = Game._collapse(unit)
         return unit
 
     @staticmethod
-    def collapse(unit):
+    def _collapse(unit):
         unit = [x for x in unit if 0 < x]
         unit += [0] * (4 - len(unit)) # pad the 0s on the right
         return unit
