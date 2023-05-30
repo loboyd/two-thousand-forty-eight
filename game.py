8171a2f7eb7c5cb0c007f62d3e83acc694fa584e
loboyd <logan.boyd@protonmail.com>
2023-05-30 15:04:55 -0400

    Make simple neural network
    This was mostly written by ChatGPT, so that's nice. Also, move
    `Game` class to `game.py`.

---
+++ game.py
@@ -0,0 +1,113 @@
+import copy
+import enum
+import random
+import sys
+import termios
+import tty
+
+class Direction(enum.Enum):
+    RIGHT = 1
+    DOWN = 2
+    UP = 3
+    LEFT = 4
+
+class State(enum.Enum):
+    ONGOING = 1
+    OVER = 2
+
+class Game:
+
+    EMPTY_BOARD = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+    def __init__(self, score=0, board=EMPTY_BOARD, state=State.ONGOING, exp=False):
+        self.score = score
+        self.board = board
+        self.state = state
+        self.exp = exp
+        self._place_random()
+        self._place_random()
+
+    def move(self, direction):
+        snapshot = copy.deepcopy(self.board)
+        if direction == Direction.RIGHT:
+            for r in range(4):
+                self.board[r] = list(reversed(Game._move_unit(reversed(self.board[r]))))
+        if direction == Direction.LEFT:
+            for r in range(4):
+                self.board[r] = Game._move_unit(self.board[r])
+        if direction == Direction.UP:
+            for c in range(4):
+                unit = [self.board[0][c], self.board[1][c], self.board[2][c], self.board[3][c]]
+                unit = Game._move_unit(unit)
+                self.board[0][c] = unit[0]
+                self.board[1][c] = unit[1]
+                self.board[2][c] = unit[2]
+                self.board[3][c] = unit[3]
+        if direction == Direction.DOWN:
+            for c in range(4):
+                unit = [self.board[3][c], self.board[2][c], self.board[1][c], self.board[0][c]]
+                unit = Game._move_unit(unit)
+                self.board[0][c] = unit[3]
+                self.board[1][c] = unit[2]
+                self.board[2][c] = unit[1]
+                self.board[3][c] = unit[0]
+        if self.board != snapshot:
+            self._place_random()
+
+    def _place_random(self):
+        empties = [(r, c) for r in range(4) for c in range(4) if self.board[r][c] == 0]
+        if empties == []:
+            self.state = State.OVER
+            return
+        r, c = random.choice(empties)
+        self.board[r][c] = 1 if random.random() < 0.9 else 2
+
+    @staticmethod
+    def _move_unit(unit):
+        unit = Game._collapse(unit)
+        for i in range(3):
+            if unit[i] > 0 and unit[i] == unit[i+1]:
+                unit[i] += 1
+                unit[i+1] = 0
+        unit = Game._collapse(unit)
+        return unit
+
+    @staticmethod
+    def _collapse(unit):
+        unit = [x for x in unit if 0 < x]
+        unit += [0] * (4 - len(unit)) # pad the 0s on the right
+        return unit
+
+    def _tile_repr(self, tile): return f'{(2**tile if tile > 0 and self.exp else tile):>4}'
+
+    def __repr__(self):
+        return "\n".join(["  ".join(self._tile_repr(x) for x in row) for row in self.board])
+
+
+def get_input_char():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setraw(fd)
+    input_char = sys.stdin.read(1)
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return input_char
+
+def play_game():
+    game = Game(exp=True)
+    for _ in range(100): print()
+    print(game)
+    while game.state == State.ONGOING:
+        move = get_input_char()
+        if move == 'j':
+            game.move(Direction.LEFT)
+        if move == 'k':
+            game.move(Direction.DOWN)
+        if move == 'l':
+            game.move(Direction.RIGHT)
+        if move == 'i':
+            game.move(Direction.UP)
+        if move == 'q':
+            exit()
+        for _ in range(100): print()
+        print(game)
+
--- main.py
+++ main.py
@@ -1,102 +1,46 @@
 #!/usr/bin/env python3
 
-import copy
-import enum
-import random
-import sys
-import termios
-import tty
+import torch
+from torch.distributions import Categorical
+import torch.nn as nn
+import torch.nn.functional as F
 
-class Direction(enum.Enum):
-    RIGHT = 1
-    DOWN = 2
-    UP = 3
-    LEFT = 4
+# Define the neural network architecture
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.fc1 = nn.Linear(16, 100)  # Fully connected layer from input to hidden
+        self.relu = nn.ReLU()  # Activation function (ReLU)
+        self.fc2 = nn.Linear(100, 4)  # Fully connected layer from hidden to output
 
-class State(enum.Enum):
-    ONGOING = 1
-    OVER = 2
-
-class Game:
-
-    EMPTY_BOARD = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-
-    def __init__(self, score=0, board=EMPTY_BOARD, state=State.ONGOING, exp=False):
-        self.score = score
-        self.board = board
-        self.state = state
-        self.exp = exp
-        self._place_random()
-        self._place_random()
-
-    def move(self, direction):
-        snapshot = copy.deepcopy(self.board)
-        if direction == Direction.RIGHT:
-            for r in range(4):
-                self.board[r] = list(reversed(Game._move_unit(reversed(self.board[r]))))
-        if direction == Direction.LEFT:
-            for r in range(4):
-                self.board[r] = Game._move_unit(self.board[r])
-        if direction == Direction.UP:
-            for c in range(4):
-                unit = [self.board[0][c], self.board[1][c], self.board[2][c], self.board[3][c]]
-                unit = Game._move_unit(unit)
-                self.board[0][c] = unit[0]
-                self.board[1][c] = unit[1]
-                self.board[2][c] = unit[2]
-                self.board[3][c] = unit[3]
-        if direction == Direction.DOWN:
-            for c in range(4):
-                unit = [self.board[3][c], self.board[2][c], self.board[1][c], self.board[0][c]]
-                unit = Game._move_unit(unit)
-                self.board[0][c] = unit[3]
-                self.board[1][c] = unit[2]
-                self.board[2][c] = unit[1]
-                self.board[3][c] = unit[0]
-        if self.board != snapshot:
-            self._place_random()
-
-    def _place_random(self):
-        empties = [(r, c) for r in range(4) for c in range(4) if self.board[r][c] == 0]
-        if empties == []:
-            self.state = State.OVER
-            return
-        r, c = random.choice(empties)
-        self.board[r][c] = 1 if random.random() < 0.9 else 2
-
-    @staticmethod
-    def _move_unit(unit):
-        unit = Game._collapse(unit)
-        for i in range(3):
-            if unit[i] > 0 and unit[i] == unit[i+1]:
-                unit[i] += 1
-                unit[i+1] = 0
-        unit = Game._collapse(unit)
-        return unit
-
-    @staticmethod
-    def _collapse(unit):
-        unit = [x for x in unit if 0 < x]
-        unit += [0] * (4 - len(unit)) # pad the 0s on the right
-        return unit
-
-    def _tile_repr(self, tile): return f'{(2**tile if tile > 0 and self.exp else tile):>4}'
-
-    def __repr__(self):
-        return "\n".join(["  ".join(self._tile_repr(x) for x in row) for row in self.board])
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = F.softmax(out, dim=1)  # Apply softmax activation to the output
+        return out
 
 
-def get_input_char():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    tty.setraw(fd)
-    input_char = sys.stdin.read(1)
-    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return input_char
+def sample_action(output):
+    distribution = Categorical(output)
+    action = distribution.sample()
+    return action.item()
 
-game = Game()
-print(game)
-game.move(Direction.RIGHT)
-print()
-print(game)
+# Create an instance of the network
+net = SimpleNet()
+
+# Generate random input data
+input_data = torch.tensor([[
+    0., 0., 0., 0.,
+    0., 0., 0., 0.,
+    0., 0., 0., 0.,
+    0., 0., 1., 1.
+]])
+
+# Pass the input through the network
+output = net(input_data)
+print(output)
+
+action = sample_action(output)
+print(action)
 
