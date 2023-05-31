1ed7d4fcac46e481951b80c830f7ee9a0c6da498
loboyd <logan.boyd@protonmail.com>
2023-05-31 13:30:20 -0400

    Create `Replay` class to store a game trajectory

--- main.py
+++ main.py
@@ -29,28 +29,55 @@ class SimpleNet(nn.Module):
         return out
 
 
+# todo: probably put this in a class or something
 def sample_action(output):
     distribution = Categorical(output)
     action = distribution.sample()
     return action.item()
 
+
+class Replay:
+    def __init__(self, net):
+        #self.seed = random.randint(0, 1023)
+        self.net = net
+        self.states = [] # as input-ready tensors
+        self.actions = []
+        self.rewards = []
+
+    def play(self, display=False):
+        game = Game()
+        if display: print(game)
+        while game.state == State.ONGOING:
+            # format input and write down state
+            flattened_board = [float(item) for sublist in game.board for item in sublist]
+            input_data = torch.tensor([flattened_board])
+            self.states += [input_data]
+
+            # pass the board state into the network
+            output = net(input_data)
+
+            # sample the output to determine which action to play, write down the action
+            action = Direction(sample_action(output) + 1)
+            self.actions += [action]
+            if display: print(action)
+            game.move(action)
+
+            # write down the current score
+            self.rewards += [game.score]
+
+            if display:
+                for _ in range(100): print()
+                print(game)
+                print(game.score)
+
+
 # Create an instance of the network
 net = SimpleNet()
-
-game = Game()
-print(game)
-while game.state == State.ONGOING:
-    # pass the board state into the network
-    flattened_board = [float(item) for sublist in game.board for item in sublist]
-    input_data = torch.tensor([flattened_board])
-    output = net(input_data)
-
-    # get action to play
-    action = Direction(sample_action(output) + 1)
-    print(action)
-    game.move(action)
-
-    for _ in range(100): print()
-    print(game)
-    print(game.score)
+replay = Replay(net)
+replay.play()
+for (state, action, reward) in zip(replay.states, replay.actions, replay.rewards):
+    print(f'state: {state}')
+    print(f'action: {action}')
+    print(f'reward: {reward}')
+    print()
 
