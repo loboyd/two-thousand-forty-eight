8b1146fafaee53114f5b7d80cfd670a18bc7e55e
Logan Boyd <logan.boyd@protonmail.com>
2023-06-06 03:27:11 -0400

    Move `Agent` and `Episdoe` into their own file

--- main.py
+++ main.py
@@ -1,97 +1,18 @@
 #!/usr/bin/env python3
 
-import pickle
 import random
 import time
 
 import torch
-from torch.distributions import Categorical
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 
-from game import Direction, Game, State
+from agent import Agent, Episode, set_seed
 
 seed = 42
 random.seed(seed)
-torch.manual_seed(seed)
-
-import pickle
-
-# Define the neural network architecture
-class Agent(nn.Module):
-    def __init__(self):
-        super(Agent, self).__init__()
-        self.fc1 = nn.Linear(16, 512)
-        self.fc2 = nn.Linear(512, 4)
-
-    def forward(self, x): return F.softmax(self.fc2(F.relu(self.fc1(x))), dim=1)
-
-    def save(self):
-        with open('data.pickle', 'wb') as file:
-            pickle.dump(self, file)
-
-    @classmethod
-    def load(cls):
-        with open('data.pickle', 'rb') as file:
-            return pickle.load(file)
-
-
-class Episode:
-    def __init__(self, net, gamma=1):
-        self.net = net
-        self.gamma = gamma
-        self.log_probs = []
-        self.rewards = []
-        self.score = 0
-
-    def run(self):
-        game = Game(exp=True)
-        ct = 0 # number of actions taken
-        while game.state == State.ONGOING:
-            # prepare input from board
-            input_data = torch.tensor([[float(tile) for row in game.board for tile in row]])
-
-            # run input through net generate policy distribution
-            distribution = Categorical(self.net(input_data))
-
-            # sample the distribution
-            sample = distribution.sample()
-
-            # write down the logprob
-            self.log_probs.append(distribution.log_prob(sample))
-
-            # convert action to direction/move
-            direction = Direction(sample.item() + 1)
-
-            # execute move
-            score = game.score
-            game.move(direction)
-            ct += 1
-
-            # write down the reward (change in score)
-            self.rewards.append(game.score - score)
-
-        # adjust rewards to represent future discounted return
-        R = 0
-        for i in range(len(self.rewards)-1, -1, -1):
-            R = self.rewards[i] + self.gamma * R
-            self.rewards[i] = R
-
-        # compute total episode loss
-        losses = [-log_prob * R for (log_prob, R) in zip(self.log_probs, self.rewards)]
-        loss = torch.cat(losses).sum()
-
-        # compute and accumulate gradient
-        loss.backward()
-
-        # write down final score
-        self.score = game.score
-
-        return ct
+set_seed(seed)
 
 # set up net and optimizer
-net = Agent()
+net = Agent.load()
 optimizer = torch.optim.Adam(net.parameters(), lr=0.0003)
 
 #for epoch in range(20): # number of epochs
@@ -114,5 +35,5 @@ while True:
     optimizer.step()
 
     net.save()
-    print(f'epoch {epoch}: [{min(scores)}, {sum(scores) / len(scores)}, {max(scores)}], {time.time() - t}s')
+    print(f'epoch {epoch}: [{min(scores):5.2f}, {(sum(scores) / len(scores)):5.2f}, {max(scores):5.2f}], {time.time() - t}s')
 
