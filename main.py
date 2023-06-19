65c40cc7453bfb4326668781d5e77213fc813551
Logan Boyd <logan.boyd@protonmail.com>
2023-06-19 18:41:25 -0400

    Update hyperparameters
    Also, remove the "median" score reporting because it had a bug
    (computing the median via `data[len(data)//2]` requires `data` to
    be sorted. Sorting takes time and average might be a better metric
    anyway. idk.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 LOAD = False
 SAVE = False
-BATCH_SIZE=2000
+BATCH_SIZE=256
 
 import random
 import time
@@ -11,7 +11,7 @@ import torch
 
 from agent import Agent, Episode, set_seed
 
-seed = 42
+seed = 40
 random.seed(seed)
 set_seed(seed)
 
@@ -51,7 +51,7 @@ def update(net, optimizer, episodes, start_time=None):
     if SAVE: net.save()
 
     min_score = min(scores)
-    med_score = scores[len(scores)//2]
+    avg_score = sum(scores) / len(scores)
     max_score = max(scores)
     dt = time.time() - start_time
 
@@ -61,14 +61,16 @@ def update(net, optimizer, episodes, start_time=None):
         grad_norm += (param.grad ** 2).sum().item()
     grad_norm = grad_norm ** 0.5
     print(f'grad norm: {grad_norm}')
-
-    print(f'[{min_score:7.2f}, {med_score:7.2f}, {max_score:7.2f}], {dt:4.2f}s')
-    print()
+    print(f'[{min_score:7.2f}, {avg_score:7.2f}, {max_score:7.2f}], {dt:4.2f}s')
 
 
+# NOTE: Look into RMSprop optimization
 # set up net and optimizer
 net = Agent.load() if LOAD else Agent()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.0003)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.005)
+#lr_decay = lambda epoch: 0.1 ** (epoch // 16)
+lr_decay = lambda epoch: max(0.0001, 0.9 ** (epoch // 4)) # decay by 90% every 32 epochs, plateau
+scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_decay)
 
 while True:
     start_time = time.time()
@@ -77,4 +79,7 @@ while True:
     episodes = [Episode(net) for _ in range(BATCH_SIZE)]
     for ep in episodes: ep.run()
     update(net, optimizer, episodes, start_time)
+    scheduler.step()
+    print(f'lr: {optimizer.param_groups[0]["lr"]}')
+    print()
 
