8d10206c7f9f5dd1541fd6d2223abe75f445d31b
Logan Boyd <logan.boyd@protonmail.com>
2023-07-02 17:57:41 -0400

    Rewrite `Episode.run()` method
    Now this is agent-agnostic.

--- episode.py
+++ episode.py
@@ -1,7 +1,6 @@
-import torch
-from torch.distributions import Categorical
+import copy
 
-from game import Direction, Game
+from game import Game
 
 class Episode:
     def __init__(self, agent, gamma=1):
@@ -12,45 +11,16 @@ class Episode:
         self.rewards = []
         self.score = 0
 
-    # todo: possibly refactor this to take advantage of the `Agent.get_move()` method (since right
-    #       now that code is largely a duplication of this code.
     def run(self):
         game = Game(exp=True)
-        ct = 0 # number of actions taken
-        while True:
-            mask = game.get_move_mask()
-            if mask == [False, False, False, False]:
-                break
-
-            # prepare input from board
-            board = torch.tensor(game.board, dtype=torch.float32)
-            mask = torch.tensor(mask, dtype=torch.float32)
-            self.states.append((board, mask))
-
-            # add a dimension for batching
-            board = board.unsqueeze(0)
-            mask = mask.unsqueeze(0)
-
-            # run input through agent generate policy distribution
-            distribution = Categorical(self.agent(board, mask))
-
-            # sample the distribution
-            sample = distribution.sample()
-
-            # convert action to direction/move
-            sample_item = sample.item()
-            direction = Direction(sample_item + 1)
-
-            # execute move
-            score = game.score
-            game.move(direction)
-            self.actions.append(sample_item)
-
-            # write down the reward (change in score)
-            self.rewards.append(game.score - score)
+        prev_score = game.score
+        while move := self.agent.get_move(game):
+            self.states.append(copy.deepcopy(game))
+            self.actions.append(move)
+            game.move(move)
+            self.rewards.append(game.score - prev_score)
 
         self._adjust_rewards()
-
         self.score = game.score
 
     def _adjust_rewards(self):
