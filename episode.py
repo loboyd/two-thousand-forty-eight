936d0ffd7e19c7fc9eedceafc3b01874fe5c9977
Logan Boyd <logan.boyd@protonmail.com>
2023-07-02 21:27:47 -0400

    Add `Batch` class for handling episode batches
    Because this makes use of Torch's batching ability, it's fast as
    fuck.

--- episode.py
+++ episode.py
@@ -29,3 +29,27 @@ class Episode:
             R = self.rewards[i] + self.gamma * R
             self.rewards[i] = R
 
+class Batch:
+    def __init__(self, agent, batch_size=1, gamma=1):
+        self.agent = agent
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.episodes = [Episode(agent) for _ in range(batch_size)]
+
+    def run(self):
+        games = [Game() for _ in range(self.batch_size)]
+
+        while z := [(ind, game) for ind, game in enumerate(games) if game.ongoing()]:
+            indices, in_play = zip(*z) # unzip (i don't understand how this works, but it does)
+            moves = self.agent.get_batch_moves(in_play)
+            for ind, game, move in zip(indices, in_play, moves):
+                self.episodes[ind].states.append(copy.deepcopy(game))
+                self.episodes[ind].actions.append(move)
+                prev_score = game.score
+                game.move(move)
+                self.episodes[ind].rewards.append(game.score - prev_score)
+                self.episodes[ind].score = game.score
+
+        for ep in self.episodes:
+            ep._adjust_rewards()
+
