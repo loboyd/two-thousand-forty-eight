059bd1d3937f9d1bd8f16ba04d485569220d790c
Logan Boyd <logan.boyd@protonmail.com>
2023-07-04 23:52:14 -0400

    Remove `main.py` and add `train.py`
    `main.py` was broken at this point anyway because of the other
    refactoring changes. The code in `train.py` should have been
    including in the source tree a while ago.

---
+++ train.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+LOAD = False
+SAVE = False
+BATCH_SIZE=2**4
+
+import random
+import time
+
+import torch
+from torch import nn
+
+from agent import Agent, set_seed
+from episode import Batch, Episode
+from game import Game
+
+seed = 41
+random.seed(seed)
+set_seed(seed)
+
+# set up agent and optimizer
+agent = Agent.load() if LOAD else Agent()
+optimizer = torch.optim.Adam(agent.parameters(), lr=0.002)
+
+while True:
+    # run a batch of episodes
+    batch = Batch(agent, batch_size=BATCH_SIZE, gamma=0.96)
+    batch.run()
+
+    # do an update
+    agent.update(optimizer, batch.episodes)
+    if SAVE: agent.save()
+
